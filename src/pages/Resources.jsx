5dfd567b67a6981863508c68617a37a441a8f5d4
Haruyuki Oosaki <23sam55781@gmail.com>
2025-06-26 23:55:26 +0900

    Add lecture PDF

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -4,8 +4,8 @@ function Resources() {
       <h2>資料・講義動画ページ</h2>
       <h1>一年生</h1>
       <ul>
-        <li><a href="public/pdfs/三角関数の加法定理セミナー資料.pdf" target="_blank">加法定理（Chatgpt）</a></li>
-        <li><a href="public/pdfs/余弦定理・正弦定理.pdf" target="_blank">余弦定理・正弦定理（chatgpt）</a></li>
+        <li><a href="/math-club-site/pdfs/三角関数の加法定理セミナー資料.pdf" target="_blank">加法定理（Chatgpt）</a></li>
+        <li><a href="/math-club-site/pdfs/余弦定理・正弦定理.pdf" target="_blank">余弦定理・正弦定理（chatgpt）</a></li>
       </ul>
       <h1>二年生</h1>
       <h1>三年生</h1>
