57e14d57aedc667d66936756e6037ad27549d6b8
Haruyuki Oosaki <23sam55781@gmail.com>
2025-07-02 21:35:01 +0900

    pdf added

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -8,6 +8,10 @@ function Resources() {
         <li><a href="/math-club-site/pdfs/余弦定理・正弦定理.pdf" target="_blank">余弦定理・正弦定理（chatgpt）</a></li>
       </ul>
       <h1>二年生</h1>
+        <ul>
+          <li><a href="/math-club-site/pdfs/微分入門セミナー 講義ノート.pdf" target="_blank">微分入門（chatgpt）</a></li>
+          <li><a href="/math-club-site/pdfs/微分入門セミナー別途 部長による補足.pdf" target="_blank">微分入門補足(部長オリジナル)</a></li>
+        </ul>
       <h1>三年生</h1>
       <h1>四年生</h1>
       <h1>五年生</h1>
