442157149181a6c6ae30f117d85fb1033cbdafaf
Haruyuki Oosaki <23sam55781@gmail.com>
2025-08-03 20:59:17 +0900

    ooo

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -10,11 +10,11 @@ function Resources() {
       <h1>二年生</h1>
         <ul>
           <li><a href="/math-club-site/pdfs/logとネイピア数.pdf" target="_blank">logとネイピア数</a>l</li>
-          <li><a href="/math-club-site/pdfs/∑って？.pdf" target="_blank">∑とは</a></li>
+          <li><a href="/math-club-site/pdfs/シグマ.pdf" target="_blank">∑とは</a></li>
           <li><a href="/math-club-site/pdfs/微分入門セミナー 講義ノート.pdf" target="_blank">微分入門（chatgpt）</a></li>
           <li><a href="/math-club-site/pdfs/微分入門セミナー別途 部長による補足.pdf" target="_blank">微分入門補足</a></li>
           <li><a href="/math-club-site/pdfs/微分セミナーII.pdf" target="_blank">微分セミナーII</a></li>
-          <li><a href="/math-club-site/pdfs/数列入門.pdf" target="_blank">数列セミナー</a>数列セミナー</li>
+          <li><a href="/math-club-site/pdfs/数列入門.pdf" target="_blank">数列セミナー</a></li>
           <li><a href="/math-club-site/pdfs/積分セミナー1.pdf" target="_blank">積分セミナー1</a></li>
           <li><a href="/math-club-site/pdfs/ネイピア数微積分.pdf" target="_blank">logとネイピア数の微積分</a></li>
           <li><a href="/math-club-site/pdfs/対数とネイピア数の微分と積分(補足）.pdf" target="_blank">logとネイピア数の微積分(補足)</a></li>
